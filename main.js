6faa9e944e830d7ab3da21b355ea0208806ca118
Abdelrahman Nabeel <abdelrahmannabeelkassar@gmail.com>
2023-06-20 12:21:23 +0300

    js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let skillsSection = document.querySelector(".our-skills");
 let theProgressSpan = document.querySelectorAll(".the-progress span");
+
 window.addEventListener("scroll", () => {
   if (window.scrollY >= skillsSection.offsetTop - 250) {
     theProgressSpan.forEach((span) => {
@@ -20,3 +21,25 @@ upBtn.onclick = function () {
     behavior: "smooth",
   });
 };
+
+let nums = document.querySelectorAll(".stats .number");
+let statsSection = document.querySelector(".stats");
+let started = false;
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= statsSection.offsetTop - 250) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+});
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
